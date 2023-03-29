ff9c74b8e39a31364ad88182621018f83cc0b6d2
Vidisha Nevatia <vidishanevatia@gmail.com>
2023-03-29 06:51:56 -0700

    commit app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+// Create a react component
+// that inputs a text area message 
+// then performs a fetch request to
+// localhost:3001 gets back a response as a data.url 
+// and displays the image in the box below 
+
+import React, { useState } from 'react';
+import './App.css'
 
 function App() {
+  const [message, setMessage] = useState('');
+  const [response, setResponse] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:3001/', {
+      method: 'POST',
+      headers:{
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({ message }), 
+    })
+    .then((res) => res.json())
+    .then((data) => setResponse(data.message));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Vidisha's Picture Generating app</h1>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={message}
+          placeholder='Ask Vidi anything!'
+          onChange={(e) => setMessage(e.target.value)}>
+        </textarea>
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+      {response && <img src={response} alt="Response" />}
+      </div>
+    </div>  
   );
 }
 
